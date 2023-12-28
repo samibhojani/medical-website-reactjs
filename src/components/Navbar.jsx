7f86a025002080b866c393f226e627ee02a44e10
Sami Bhojani <msammi96@gmail.com>
2023-12-28 22:06:48 +0500

    styling NavBar and its components for smaller screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-scroll'
+import {AiOutlineClose , AiOutlineMenu } from 'react-icons/ai'
+
 
 const Navbar = () => {
-  return (
+
+    const [menu, setMenu] = useState(false);
+
+
+    const handleChange = () => {
+        setMenu(!menu);
+    };
+
+    const closeMenu = () => {
+        setMenu(false);
+    };
+
+    return (
     <>
       <div className='fixed w-full z-10 text-white'>
         <div className='flex flex-row justify-between p-5 md:px-32 px-5 bg-backgroundColor shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
@@ -21,6 +35,25 @@ const Navbar = () => {
             <div className='hidden lg:flex'>
                 <button className='bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out' >Contact us</button>
             </div>
+
+            <div className='lg:hidden flex items-center'>
+                {menu ? (
+                    <AiOutlineClose size={28} onClick={handleChange} />
+                ): (
+                    <AiOutlineMenu size={28} onClick={handleChange} />
+                )}
+            </div>
+        </div>
+        <div className={`${menu ? "translate-x-0" : "translate-x-full"} lg:hidden flex flex-col absolute bg-backgroundColor text-white left-0 top-16 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform`}>
+                <Link to='home' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Home</Link>
+                <Link to='about' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>About us</Link>
+                <Link to='services' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Services</Link>
+                <Link to='doctors' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Doctors</Link>
+                <Link to='blog' spy={true} smooth={true} duration={500} className='hover:text-hoverColor transition-all cursor-pointer'>Blog</Link>
+                <div className='lg:hidden'>
+                <button className='bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out' >Contact us</button>
+                </div>
+
         </div>
       </div>
     </>
